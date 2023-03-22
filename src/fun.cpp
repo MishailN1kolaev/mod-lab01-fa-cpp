bf31acbcf34f8d4c66272df45ee64086c2bb69dc
MishailN1kolaev <114501160+MishailN1kolaev@users.noreply.github.com>
2023-03-22 16:51:04 +0300

    update 5

--- src/fun.cpp
+++ src/fun.cpp
@@ -25,7 +25,7 @@ unsigned int faStr1(const char* str) {
             }
             in_word = false;
             has_digit = false;
-            }
+        }
         str++;
     }
 
@@ -46,23 +46,31 @@ unsigned int faStr2(const char* str) {
             in = 1;
             F = 1;
         }
-        else if ((in == 1) && (str[i] != ' ') && (F == 1)) {
-            if ((str[i] >= 65) && (str[i] <= 90)) F = 0;
-            else if ((str[i] > 122) || (str[i] < 97)) F = 0;
-        }
-        else if (in == 1 && str[i] == ' ') {
-            if (F == 1) count++;
-            F = 0;
-            in = 0;
-        }
-        else if ((in == 1) && (F == 1) && (str[i + 1] == '\0')) {
-            count++;
+        else if (in == 1) {
+            if ((str[i] != ' ') && (F == 1)) {
+                if ((str[i] >= 65) && (str[i] <= 90)) {
+                    F = 0;
+                }
+                else if ((str[i] > 122) || (str[i] < 97)) {
+                    F = 0;
+                }
+            }
+            else if (str[i] == ' ') {
+                if (F == 1) {
+                    count++;
+                }
+                F = 0;
+                in = 0;
+            }
+            else if ((F == 1) && (str[i + 1] == '\0')) {
+                count++;
+            }
         }
         i++;
     }
+
     return count;
 }
-
 unsigned int faStr3(const char* str) {
     unsigned int word_count = 0;
     unsigned int character_count = 0;
@@ -71,26 +79,29 @@ unsigned int faStr3(const char* str) {
     for (int i = 0; str[i] != '\0'; ++i) {
         char c = str[i];
 
-        if ((c >= 'A') && (c <= 'Z')) {
+        if (c >= 'A' && c <= 'Z') {
             in_word = true;
             character_count++;
         }
-        else if ((c >= 'a') && (c <= 'z')) {
-            in_word = true;
-            character_count++;
+        else {
+            if (c >= 'a' && c <= 'z') {
+                in_word = true;
+                character_count++;
+            }
+            else {
+                if (in_word) {
+                    word_count++;
+                    in_word = false;
+                }
+            }
         }
-        else if (in_word) {
+
+        if (in_word) {
             word_count++;
-            in_word = false;
         }
+
+        float average = static_cast<float>(character_count) / static_cast<float>(word_count);
+        int rounded_average = static_cast<int>(round(average));
+
+        return rounded_average;
     }
-
-    if (in_word) {
-        word_count++;
-    }
-
-    float average = static_cast<float>(character_count) / static_cast<float>(word_count);
-    int rounded_average = static_cast<int>(round(average));
-
-    return rounded_average;
-}
